6d291505914204896a19cfa99f26724cc457c0fb
Lee Mendelowitz <Lee.Mendelowitz@gmail.com>
2015-03-22 08:58:32 -0400

    Addex .tmux.conf

--- env.sh
+++ env.sh
@@ -1,34 +1,49 @@
 #!/bin/bash
+# Set up bash environment with useful aliases and functions
+###########################################################
+
 alias ls="ls --color=always"
 alias lsd="ls -l | grep '^d'"
 alias less="less -iR"
 alias grep="grep --color"
 alias serve="python -m SimpleHTTPServer"
 alias ports="sudo lsof -i | grep -E '(ESTABLISHED|LISTEN)'"
+alias PATH='echo $PATH | tr ":" "\n"'
 
 stty -ixon # Fix Ctrl+S for the Ctrl+R reverse search
 
  ~/config/git-completion.bash
 
-export PATH=~/config/bin:$PATH
+export PATH=~/config/bin:$PATH # Add our bin directory to path
 export EDITOR=`which vi`
 
-
-
-# Awesome Functions!
+################################################################################
+# These find source functions can be used to grep in source files. For example:
+#
+#    find_c_source "find_alignments"
+#
 find_c_source () {
     find -name '*.c' -o -name '*.cpp' -o -name '*.cc' -o -name '*.h' | xargs -I {} grep -Hn --color $1 {}
 }
+export -f find_c_source
 
 find_py_source () {
     find -name '*.py' | xargs -I {} grep -Hn --color $1 {}
 }
 
+find_R_source () {
+    find -name '*.R' | xargs -I {} grep -Hn --color $1 {}
+}
+export -f find_R_source
+
 find_source () {
     find -name '*.sh' -o -name '*.py' -o -name '*.R' -o -name '*.c' -o -name '*.cpp' -o -name '*.cc' -o -name '*.h' | xargs -I {} grep -Hn --color $1 {}
 }
+export -f find_source
+################################################################################
 
 
+################################################################################
 # Preserve the header in output, and perform action only on body
 # Example:
 #     ps -ef | body grep "python"
@@ -38,3 +53,4 @@ body() {
     printf '%s\n' "$header"
     "$@"
 }
+export -f body
